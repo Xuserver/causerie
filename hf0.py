8ee191ae457de616777dff822ee25f663c95b5f1
Gael JAUNIN <gael.jaunin@gmail.com>
2024-02-15 12:45:32 +0100

    rename

--- hf0.py
+++ hf0.py
@@ -1,9 +1,7 @@
 from transformers import pipeline
 pipe = pipeline("translation_en_to_fr", model="t5-base")
 
-text = f"""Solar Horizon is an innovative photovoltaic project aimed at harnessing the power of solar energy to create a sustainable and eco-friendly 
-solution for energy production. Located in Tarnos, this state-of-the-art solar facility encompasses cutting-edge photovoltaic technology to convert sunlight into electricity efficiently.
-The project incorporates the latest advancements in solar cell technology, utilizing high-efficiency photovoltaic EMS to maximize energy conversion and output.
+text = f"""Microsoft Corporation is an American multinational technology corporation headquartered in Redmond, Washington. Microsoft's best-known software products are the Windows line of operating systems, the Microsoft 365 suite of productivity applications, and the Edge web browser. Its flagship hardware products are the Xbox video game consoles and the Microsoft Surface lineup of touchscreen personal computers. Microsoft ranked No. 14 in the 2022 Fortune 500 rankings of the largest United States corporations by total revenue; it was the world's largest software maker by revenue as of 2022. It is considered one of the Big Five American information technology companies, alongside Alphabet (parent company of Google), Amazon, Apple, and Meta (parent company of Facebook).
 """
 traduction = pipe(text)[0]['translation_text']
 print(traduction)
