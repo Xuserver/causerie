d87904a3dc79a994c6fed3d114851e8487105ee4
Gael JAUNIN <gael.jaunin@gmail.com>
2024-03-12 15:59:29 +0100

    mise à jour script

--- vision.py
+++ vision.py
@@ -1,25 +1,26 @@
 import base64
 import requests
- 
-# OpenAI API Key
-api_key = ""
- 
-# Function to encode the image
+
+# Function pour encoder une image en base 64 (aspect purement technique, sans importance)
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
- 
-# Path to your image
-image_path = "C:/Users/gael.jaunin/OneDrive - Naldeo/Images/bla.jpg"
- 
-# Getting the base64 string
+
+# Question "métier" 
+question = "Quelles non conformités peut on voir sur cette photo de chantier ?"
+
+# OpenAI API Key
+api_key = ""
+
+# Chemin vers une photo d'une visite de chantier
+image_path = "photos/etancheite.jpg"
 base64_image = encode_image(image_path)
- 
+
+# On fait appel à OpenAI pour analyser une photo
 headers = {
   "Content-Type": "application/json",
   "Authorization": f"Bearer {api_key}"
 }
- 
 payload = {
   "model": "gpt-4-vision-preview",
   "messages": [
@@ -28,7 +29,7 @@ payload = {
       "content": [
         {
           "type": "text",
-          "text": "Quelles non conformités peut on voir sur cette photo de chantier ?"
+          "text": question
         },
         {
           "type": "image_url",
@@ -39,7 +40,11 @@ payload = {
       ]
     }
   ],
-  "max_tokens": 300
+  "max_tokens": 300 # paramétrable
 }
 response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
 print(response.json())
+
+# Le resultat (json) pour une seule photo est trés facilement automatisable pour générer le chapitre 
+# conformité de chantier dans un compte rendu, directement au format word.
+
